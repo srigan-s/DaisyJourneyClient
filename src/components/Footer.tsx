c3f5424745293f71a0b5fb2f31a0e05ecaaa2756
Srigan Sivagnanenthirarajah <srigan.siva@gmail.com>
2025-08-04 17:30:14 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Heart, Linkedin, Instagram, Mail, Phone, MapPin } from 'lucide-react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-primary-900 text-white pt-2 pb-8">
+    <footer className="bg-primary-900 text-white pt-20 pb-8">
       <div className="container mx-auto px-4 md:px-6">
         <div className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-16">
