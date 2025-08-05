91c70251c70b47fe554d06a5a159ed95da6b5da1
Srigan Sivagnanenthirarajah <srigan.siva@gmail.com>
2025-08-05 02:03:27 -0400

    Updated Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -94,7 +94,7 @@ const Events: React.FC = () => {
               </div>
               <div className="relative h-64 md:h-auto">
                 <img
-                  src="images/lightskin.jpg"
+                  src="/images/lightskin.jpg"
                   alt="Volunteer Program"
                   className="w-full h-full object-cover"
                 />
