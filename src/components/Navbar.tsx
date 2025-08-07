08a84013cf5390ef89fe6db069065566449cf377
Srigan Sivagnanenthirarajah <srigan.siva@gmail.com>
2025-08-06 20:50:43 -0400

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, Linkedin, Instagram } from 'lucide-react';
+import { Menu, X, Linkedin, Instagram } from 'lucide-react':
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
